7d3eb430dd3d02cb0a21f21ebe7f051c7acc66b5
Dhageu <125014642+Dhageu@users.noreply.github.com>
2023-03-04 15:34:46 +0300

    Add files via upload

---
+++ 2/figure/main3/main3/main3.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include "figure.h"
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float x1, x2, x3, x4, y1, y2, y3, y4;
+    cout << "\t" << "Четырехугольник №1" << endl << endl;
+    cout << "Введите x1, x2, x3, x4, y1, y2, y3, y4: ";
+    cin >> x1 >> x2 >> x3 >> x4 >> y1 >> y2 >> y3 >> y4;
+    cout << endl;
+    figure first(x1, x2, x3, x4, y1, y2, y3, y4);
+    first.show();
+
+    cout << "\t" << "Четырехугольник №2" << endl << endl;
+    cout << "Введите x1, x2, x3, x4, y1, y2, y3, y4: ";
+    cin >> x1 >> x2 >> x3 >> x4 >> y1 >> y2 >> y3 >> y4;
+    cout << endl;
+    figure second(x1, x2, x3, x4, y1, y2, y3, y4);
+    second.show();
+
+    cout << "\t" << "Четырехугольник №3" << endl << endl;
+    cout << "Введите x1, x2, x3, x4, y1, y2, y3, y4: ";
+    cin >> x1 >> x2 >> x3 >> x4 >> y1 >> y2 >> y3 >> y4;
+    cout << endl;
+    figure third(x1, x2, x3, x4, y1, y2, y3, y4);
+    third.show();
+}
